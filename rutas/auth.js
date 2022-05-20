d2f7de4098f3e191d68649b6e9b5e2b9cf2a3375
cabrerarodrigo <shoppingify.sjr@gmail.com>
2022-05-20 15:35:18 -0300

    correcciones

--- rutas/auth.js
+++ rutas/auth.js
@@ -1,13 +1,18 @@
 import { Router } from 'express'
 import passport from 'passport'
 import checkAuth from '../passport/checkAuth.js'
+import __dirname from '../dirname.js'
 import { login, register, logout, getUserData } from '../logica/auth.js'
 
 const router = Router()
 
 router.get('/', checkAuth, getUserData)
-router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), login)
+router.get('/login', (req, res) => res.sendFile('/public/login.html', {root: __dirname}))
+router.post('/login', passport.authenticate('local', { failureRedirect: '/auth/login' }), login)
+router.get('/register', (req, res) => res.sendFile('/public/register.html', {root: __dirname}))
 router.post('/register', register)
 router.get('/logout', logout)
+router.get('/profile', checkAuth, (req, res) => res.sendFile('/public/profile.html', {root: __dirname}))
+router.get('/admin', (req, res) => res.sendFile('/public/admin.html', {root: __dirname}))
 
 export default router
