407e29178c03ecef768955128ea46abdfc110b82
cabrerarodrigo <shoppingify.sjr@gmail.com>
2022-06-29 18:42:10 -0300

    separar controllers de logica

--- logica/carrito.js
+++ logica/carrito.js
@@ -3,54 +3,28 @@ import carritos from '../persistencia/daos/mongo/carrito.js'
 import { enviarNuevoPedido } from '../messaging/emails.js'
 import { avisarNuevoPedido, enviarSmsAlUsuario } from '../messaging/phoneMessages.js'
 
-export const eliminarCarrito = async (req, res, next) => {
-    try{
-        const deletedCarrito = await carritos.empty(req.user._id)
-        res.status(200).json({ message: 'Carrito eliminado correctamente' })
-    } catch(err) {
-        next(err)
-    }
-}
-export const obtenerLista = async (req, res, next) => {
-    try{
-        const listaIds = await carritos.getProducts(req.user._id)
-        const listaProductos = await productos.getMany(listaIds)
-        if(!listaProductos) return res.sendStatus(500)
-        res.status(200).json(listaProductos)
-    } catch(err) {
-        next(err)
-    }
-}
-export const agregarProductos = async (req, res, next) => {
-    try{
-        const userInput = req.body.productos
-        const nuevoCarrito = await carritos.addProducts(req.user._id, userInput)
-        res.status(200).json(nuevoCarrito)
-    } catch(err) {
-        next(err)
-    }
-}
-export const quitarProducto = async (req, res, next) => {
-    try{
-        const id_prod = req.params.id_prod
-        const deletedProduct = await carritos.removeProduct(req.user._id, id_prod)
-        res.status(200).json(deletedProduct)
-    } catch(err) {
-        next(err)
-    }
+export const eliminarProductosDelCarrito = async (userId) => {
+    const deletedCarrito = await carritos.empty(req.user._id)
+    return deletedCarrito ? true : false
 }
 
-export const realizarCompra = async (req, res) => {
-    try {
-        const listaIds = await carritos.getProducts(req.user._id)
-        const listaProductos = await productos.getMany(listaIds)
-        await enviarNuevoPedido({productos: listaProductos, user: req.user})
-        await avisarNuevoPedido({productos: listaProductos, user: req.user})
-        await enviarSmsAlUsuario(req.user)
-        await carritos.empty(req.user._id)
-        res.status(200).json({ message: 'Compra realizada con éxito' })
-    }
-    catch(err) {
-        next(err)
-    }
+export const listarProductosDelCarrito = async (userId) => {
+    const listaIds = await carritos.getProducts(userId)
+    return await productos.getMany(listaIds)
+}
+
+export const agregarProductosYDevolverCarrito = async (userId, productos) => {
+    return await carritos.addProducts(userId, productos)
+}
+export const quitarProductoDelCarrito = async (userId, productId) => {
+    return await carritos.removeProduct(userId, productId)
+}
+
+export const realizarCompraYVaciarCarrito = async (user) => {
+    const listaIds = await carritos.getProducts(user._id)
+    const listaProductos = await productos.getMany(listaIds)
+    await enviarNuevoPedido({productos: listaProductos, user: user})
+    await avisarNuevoPedido({productos: listaProductos, user: user})
+    await enviarSmsAlUsuario(user)
+    return await carritos.empty(user._id)
 }
