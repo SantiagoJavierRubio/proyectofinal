357ff6727cd0de70a24975c198b2cddb15f5018a
cabrerarodrigo <shoppingify.sjr@gmail.com>
2022-07-01 18:06:11 -0300

    remove all redirects

--- passport/checkAuth.js
+++ passport/checkAuth.js
@@ -1,6 +1,8 @@
 const checkAuth = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    return res.redirect('/auth/login');
+    return res
+      .status(401)
+      .json({ message: 'Usuario no logueado' });
   }
   if(req.user.email === 'admin@admin') req.isAdmin = true;
   next();
