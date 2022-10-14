5d36e216e3e9261dcf3c34bb1efd95eae4be32ba
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-10-14 13:52:37 -0300

    nodemailer functions

--- rutas/carrito.js
+++ rutas/carrito.js
@@ -3,7 +3,9 @@ import checkAuth from '../passport/checkAuth.js'
 import { eliminarCarrito,
     obtenerLista,
     agregarProductos,
-    quitarProducto} from '../logica/carrito.js'
+    quitarProducto,
+    realizarCompra
+} from '../logica/carrito.js'
 
 const router = Router()
 
@@ -12,5 +14,6 @@ router.delete('/', eliminarCarrito)
 router.get('/productos', obtenerLista)
 router.post('/productos', agregarProductos)
 router.delete('/productos/:id_prod', quitarProducto)
+router.post('/checkout', realizarCompra)
 
 export default router
