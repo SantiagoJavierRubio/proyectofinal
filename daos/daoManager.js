52b154de30086ad47b17a94c7ab4e37753463e0b
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-10-14 13:40:12 -0300

    firebase dao

--- daos/daoManager.js
+++ daos/daoManager.js
@@ -11,11 +11,16 @@ const getDaos = async () => {
             const { default: ProductosArchivo } = await import('./archivo/productos.js')
             return { carritos: new CarritoArchivo(), productos: new ProductosArchivo() }
         case 'MONGODB':
+            const { default: mongoose } = await import('mongoose')
+            mongoose.connect('mongodb://localhost:27017/ecommerce')
             const { default: CarritoMongo } = await import('./mongo/carrito.js')
             const { default: ProductosMongo } = await import('./mongo/productos.js')
             return { carritos: new CarritoMongo(), productos: new ProductosMongo() }
         case 'FIREBASE':
-            break
+            const { default: db } = await import('../Database/Firebase/initializeDb.js')
+            const { default: CarritoFirebase } = await import('./firebase/carrito.js')
+            const { default: ProductosFirebase } = await import('./firebase/productos.js')
+            return { carritos: new CarritoFirebase(db), productos: new ProductosFirebase(db) } 
         default:
             throw new Error('Error al cargar la data: Variable de entorno no encontrada')
     }
