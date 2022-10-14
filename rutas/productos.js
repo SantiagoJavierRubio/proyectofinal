6643226edcfa1a9cd9fc6bedc4016109b9c87867
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-10-14 13:51:06 -0300

    setup basico

---
+++ rutas/productos.js
@@ -0,0 +1,24 @@
+import { Router } from 'express'
+
+const revisarAutorizacion = (req, res, next) => {
+    // Aquí iría la lógica para revisar si el usuario está o no autorizado
+    const auth = true
+    // -- //
+    if(auth) {
+        return next()
+    } else {
+        res.status(401).json({
+            error: -1,
+            description: `ruta '${req.url} método ${req.method} no autorizada`
+        })
+    }
+}
+
+const router = Router()
+
+router.get('/:id', getAll)
+router.post('/', revisarAutorizacion, nuevoProducto)
+router.put('/:id', revisarAutorizacion, editarProducto)
+router.delete('/:id', revisarAutorizacion, eliminarProducto)
+
+export default router
